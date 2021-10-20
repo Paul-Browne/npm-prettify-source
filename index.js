2e0579a6e06dc3ea933290c27a47f19de36b012b
Paul Browne <paul.browne@newthings.co>
2021-10-20 22:59:57 +0300

    bump

--- index.js
+++ index.js
@@ -5,47 +5,50 @@ import fileWalker from "recursive-file-walker";
 import tscl from "time-stamped-console-log";
 const {check, format} = prettier;
 export default async directory => {
-  await fileWalker({
+  const files = await fileWalker({
+    id: "prettify-source",
     entry: directory,
-    onFile: async info => {
-      // only prettify files changed from the last time
-      if (info.modified) {
-        let parser;
-        if (lookup(info.path) === "text/html") {
-          parser = "html";
-        } else if (
-          lookup(info.path) === "text/css" ||
-          lookup(info.path) === "text/x-scss" ||
-          lookup(info.path) === "text/x-sass" ||
-          lookup(info.path) === "text/less"
-        ) {
-          parser = "css";
-        } else if (
-          lookup(info.path) === "application/javascript" &&
-          !/\.min\.js$/.test(info.path)
-        ) {
-          parser = "babel";
-        } else if (lookup(info.path) === "application/json") {
-          parser = "json";
-        }
-        // only prettify for valid file types
-        if (parser) {
-          const contents = await readFile(info.path, "utf8");
-          const checker = check(contents, { parser: parser });
-          // only prettify when needed
-          if (!checker) {
-            await writeFile(
-              info.path,
-              format(contents, { parser: parser })
-            );
-            tscl("prettified: " + info.path, {
-              message: {
-                color: "magenta",
-              },
-            });
-          }
+    flatten: true,
+    readFiles: "modified"
+  });
+  files.forEach(async file => {
+    // only prettify files changed from the last time
+    if (file.modified) {
+      let parser;
+      if (lookup(file.path) === "text/html") {
+        parser = "html";
+      } else if (
+        lookup(file.path) === "text/css" ||
+        lookup(file.path) === "text/x-scss" ||
+        lookup(file.path) === "text/x-sass" ||
+        lookup(file.path) === "text/less"
+      ) {
+        parser = "css";
+      } else if (
+        lookup(file.path) === "application/javascript" &&
+        !/\.min\.js$/.test(file.path)
+      ) {
+        parser = "babel";
+      } else if (lookup(file.path) === "application/json") {
+        parser = "json";
+      }
+      // only prettify for valid file types
+      if (parser) {
+        const contents = await readFile(file.path, "utf8");
+        const checker = check(contents, { parser: parser });
+        // only prettify when needed
+        if (!checker) {
+          await writeFile(
+            file.path,
+            format(contents, { parser: parser })
+          );
+          tscl("prettified: " + file.path, {
+            message: {
+              color: "magenta",
+            },
+          });
         }
       }
-    },
-  });
+    }
+  })
 };
--- test/untitled.js
+++ test/untitled.js
@@ -1,51 +1,54 @@
-const prettier = require("prettier");
-const mime = require("mime-types");
-const fs = require("fs");
-const fileWalker = require("recursive-file-walker");
-const tscl = require("time-stamped-console-log");
-
-module.exports = (directory) => {
-  fileWalker({
+import prettier from "prettier";
+import { lookup } from "mime-types";
+import { readFile, writeFile } from "fs/promises";
+import fileWalker from "recursive-file-walker";
+import tscl from "time-stamped-console-log";
+const {check, format} = prettier;
+export default async directory => {
+  const files = await fileWalker({
+    id: "prettify-source",
     entry: directory,
-    onFile: (info) => {
-      // only prettify files changed from the last time
-      if (info.modified) {
-        let parser;
-        if (mime.lookup(info.path) === "text/html") {
-          parser = "html";
-        } else if (
-          mime.lookup(info.path) === "text/css" ||
-          mime.lookup(info.path) === "text/x-scss" ||
-          mime.lookup(info.path) === "text/x-sass" ||
-          mime.lookup(info.path) === "text/less"
-        ) {
-          parser = "css";
-        } else if (
-          mime.lookup(info.path) === "application/javascript" &&
-          !/\.min\.js$/.test(info.path)
-        ) {
-          parser = "babel";
-        } else if (mime.lookup(info.path) === "application/json") {
-          parser = "json";
-        }
-        // only prettify for valid file types
-        if (parser) {
-          const contents = fs.readFileSync(info.path, "utf8");
-          const checker = prettier.check(contents, { parser: parser });
-          // only prettify when needed
-          if (!checker) {
-            fs.writeFileSync(
-              info.path,
-              prettier.format(contents, { parser: parser })
-            );
-            tscl("prettified: " + info.path, {
-              message: {
-                color: "magenta",
-              },
-            });
-          }
+    flatten: true,
+    readFiles: "modified"
+  });
+  files.forEach(async file => {
+    // only prettify files changed from the last time
+    if (file.modified) {
+      let parser;
+      if (lookup(file.path) === "text/html") {
+        parser = "html";
+      } else if (
+        lookup(file.path) === "text/css" ||
+        lookup(file.path) === "text/x-scss" ||
+        lookup(file.path) === "text/x-sass" ||
+        lookup(file.path) === "text/less"
+      ) {
+        parser = "css";
+      } else if (
+        lookup(file.path) === "application/javascript" &&
+        !/\.min\.js$/.test(file.path)
+      ) {
+        parser = "babel";
+      } else if (lookup(file.path) === "application/json") {
+        parser = "json";
+      }
+      // only prettify for valid file types
+      if (parser) {
+        const contents = await readFile(file.path, "utf8");
+        const checker = check(contents, { parser: parser });
+        // only prettify when needed
+        if (!checker) {
+          await writeFile(
+            file.path,
+            format(contents, { parser: parser })
+          );
+          tscl("prettified: " + file.path, {
+            message: {
+              color: "magenta",
+            },
+          });
         }
       }
-    },
-  });
+    }
+  })
 };
